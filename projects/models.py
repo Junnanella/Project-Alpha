f9a9691e84a6c1a4f54f8e5821fcebf37e4cd762
Junella Caringal <junellacaringal@gmail.com>
2022-05-09 14:57:13 -0700

    Feature 4 complete

--- projects/models.py
+++ projects/models.py
@@ -4,6 +4,8 @@ from django.conf import settings
 USER_MODEL = settings.AUTH_USER_MODEL
 
 # Create your models here.
+
+
 class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
