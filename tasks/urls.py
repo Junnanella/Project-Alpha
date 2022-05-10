ea3069c7c05abf109a652798b5f3a1a42d6039d7
Junella Caringal <junellacaringal@gmail.com>
2022-05-10 14:01:54 -0700

    Feature 17 almost complete. Need to pass one more test.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskCreateView, TaskListView
+from .views import TaskCreateView, TaskListView, TaskUpdateView
 
 # app_name = "tasks"
 
 urlpatterns = [
  path("create/", TaskCreateView.as_view(), name="create_task"),
- path("mine/", TaskListView.as_view(), name="show_my_tasks")
+ path("mine/", TaskListView.as_view(), name="show_my_tasks"),
+ path("<int:pk>/complete/", TaskUpdateView.as_view(), name="complete_task")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,9 @@
+from cmath import log
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from .models import Task
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
 
 # Create your views here.
 
@@ -26,3 +27,10 @@ class TaskListView(LoginRequiredMixin,ListView):
     def get_queryset(self):
         return Task.objects.filter(assignee=self.request.user)
     
+class TaskUpdateView(LoginRequiredMixin,UpdateView):
+    model = Task
+    template_name = "tasks/tasks_list.html"
+    fields = ["is_complete"]
+
+    success_url = reverse_lazy("show_my_tasks")
+        
